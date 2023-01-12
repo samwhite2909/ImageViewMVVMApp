fdd5ee9b87ccd3d574eebcd0be3f686d907d6222
samwhite2909 <Bulldopzer98>
2023-01-12 17:20:05 +0000

    Now shows the images and allows the user to search based on a search term.

--- app/src/main/java/com/swhite/imagesearchapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/swhite/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -1,10 +1,14 @@
 package com.swhite.imagesearchapp.ui.gallery
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.swhite.imagesearchapp.R
+import com.swhite.imagesearchapp.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,11 +16,59 @@ class GalleryFragment: Fragment(R.layout.fragment_gallery) {
 
     private val viewModel by viewModels<GalleryViewModel> ()
 
+    private var _binding : FragmentGalleryBinding? = null
+    private val binding get() = _binding!!
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.photos.observe(viewLifecycleOwner) {
+        _binding = FragmentGalleryBinding.bind(view)
 
+        val adapter = UnsplashPhotoAdapter()
+
+        binding.apply {
+            recyclerView.setHasFixedSize(true)
+            recyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
+                header = UnsplashLoadPhotoStateAdapter { adapter.retry() },
+                footer = UnsplashLoadPhotoStateAdapter { adapter.retry() },
+            )
         }
+
+        viewModel.photos.observe(viewLifecycleOwner) {
+            adapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.menu_gallery, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+
+                if(query != null) {
+                    binding.recyclerView.scrollToPosition(0)
+                    viewModel.searchPhotos(query)
+                    searchView.clearFocus()
+                }
+
+                return true
+
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean { return true }
+
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
